022f8ceeb4d3eb7879aa48ab6b6b40a96ec92457
valentino <v.arballo@itecriocuarto.org.ar>
2023-08-20 18:21:49 -0300

    s

---
+++ src/components/testimonio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Testimonio () {
+    return (
+        <div className="contenedor">
+            <img 
+                className="imagen"
+                src=""
+            />
+            
+            <h1>golaso</h1>
+        </div>
+    );
+}
